e873995773d156287fa5d805eeeb74225b53feb8
blaicb99 <blaicb99@gmail.com>
2025-12-02 19:02:08 +0000

    Arreglada la posició del workflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.laliga.advent',
-  appName: "Calendari d'Advent",
-  webDir: 'dist'
+  appName: 'LaLiga Advent',
+  webDir: 'dist', // <--- ASSEGURA'T QUE POSA 'dist' I NO 'www'
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
