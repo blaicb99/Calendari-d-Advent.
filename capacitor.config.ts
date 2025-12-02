91dbb183ee294514e01effc9019f24b40f4bf33d
blaicb99 <blaicb99@gmail.com>
2025-12-02 18:56:46 +0000

    Afegit Android i accio de build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laliga.advent',
+  appName: "Calendari d'Advent",
+  webDir: 'dist'
+};
+
+export default config;
